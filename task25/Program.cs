77cb7b491eb74ce583df05e5e75d0adc86810433
Pavel Danilov <danilov211@mail.ru>
2023-02-07 22:06:08 +0300

    start task25

---
+++ task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine();
+int num1 = int.Parse(Console.ReadLine());
+int MyMethod(int num)
+{
+    int i;
+    for (i = 1; i < 10; i++)
+        Console.Write(i);
+    {
+        if (i == 5) // прервать на шаге 5
+        {
+            Console.WriteLine();
+
+        }
+    }
+    return i;
+}
+MyMethod(num1);
